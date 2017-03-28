a59a5902a120ee07afe3a3a7407895716d338338
Sergio H. Costa <sergiohcosta@gmail.com>
2017-03-28 19:04:21 -0300

    itens de jardinagem adicionados

--- js/itens.js
+++ js/itens.js
@@ -452,7 +452,115 @@ itens = {
                 "qtde": "1"
             }
         ]
+    },
+    "grama": {
+        "fonte": "jardinagem",
+        "tempo": "1080",
+        "produzir": 0,
+        "armazem": 0,
+        "requisito": [
+            {
+                "nome": "semente",
+                "qtde": "1"
+            },
+            {
+                "nome": "pa",
+                "qtde": "1"
+            }
+        ]
+    },
+    "arvore": {
+        "fonte": "jardinagem",
+        "tempo": "1080",
+        "produzir": 0,
+        "armazem": 0,
+        "requisito": [
+            {
+                "nome": "semente",
+                "qtde": "2"
+            },
+            {
+                "nome": "pa",
+                "qtde": "1"
+            }
+        ]
+    },
+    "moveljardim": {
+        "fonte": "jardinagem",
+        "tempo": "1080",
+        "produzir": 0,
+        "armazem": 0,
+        "requisito": [
+            {
+                "nome": "plastico",
+                "qtde": "2"
+            },
+            {
+                "nome": "tecido",
+                "qtde": "2"
+            },
+            {
+                "nome": "tabua",
+                "qtde": "2"
+            }
+        ]
+    },
+    "fogueira": {
+        "fonte": "jardinagem",
+        "tempo": "1080",
+        "produzir": 0,
+        "armazem": 0,
+        "requisito": [
+            {
+                "nome": "tijolo",
+                "qtde": "2"
+            },
+            {
+                "nome": "cimento",
+                "qtde": "2"
+            },
+            {
+                "nome": "pa",
+                "qtde": "1"
+            }
+        ]
+    },
+    "cortador": {
+        "fonte": "jardinagem",
+        "tempo": "1080",
+        "produzir": 0,
+        "armazem": 0,
+        "requisito": [
+            {
+                "nome": "metal",
+                "qtde": "3"
+            },
+            {
+                "nome": "eletronico",
+                "qtde": "1"
+            },
+            {
+                "nome": "tinta",
+                "qtde": "1"
+            }
+        ]
+    },
+    "gnomo": {
+        "fonte": "jardinagem",
+        "tempo": "1080",
+        "produzir": 0,
+        "armazem": 0,
+        "requisito": [
+            {
+                "nome": "cimento",
+                "qtde": "2"
+            },
+            {
+                "nome": "cola",
+                "qtde": "1"
+            }
+        ]
     }
-    
-    
+
+
 };
